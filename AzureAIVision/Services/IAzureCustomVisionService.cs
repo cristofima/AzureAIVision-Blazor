99700d1d475a162c11532bf58f3003b009f37162
Cristopher Coronado Moreira <cristofima@hotmail.com>
2024-04-30 15:38:30 -0500

    Add project

---
+++ AzureAIVision/Services/IAzureCustomVisionService.cs
@@ -0,0 +1,40 @@
+﻿using AzureAIVision.Options;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction.Models;
+using Microsoft.Extensions.Options;
+
+namespace AzureAIVision.Services
+{
+    public interface IAzureCustomVisionService
+    {
+        IList<PredictionModel> ClassifyImage(Stream image);
+        IList<PredictionModel> DetectObjects(Stream image);
+    }
+
+    public class AzureCustomVisionService : IAzureCustomVisionService
+    {
+        private readonly CustomVisionPredictionClient predictionApi;
+        private readonly AzureVisionSettings settings;
+
+        public AzureCustomVisionService(IOptions<AzureVisionSettings> options)
+        {
+            this.settings = options.Value;
+            this.predictionApi = new CustomVisionPredictionClient(new ApiKeyServiceClientCredentials(this.settings.Classification.PredictionKey))
+            {
+                Endpoint = this.settings.Classification.Endpoint
+            };
+        }
+
+        public IList<PredictionModel> ClassifyImage(Stream image)
+        {
+            var result = this.predictionApi.ClassifyImage(Guid.Parse(this.settings.Classification.ProjectId), this.settings.Classification.PublishedModelName, image);
+            return result.Predictions;
+        }
+
+        public IList<PredictionModel> DetectObjects(Stream image)
+        {
+            var result = this.predictionApi.DetectImage(Guid.Parse(this.settings.Detection.ProjectId), this.settings.Detection.PublishedModelName, image);
+            return result.Predictions;
+        }
+    }
+}
---
+++ AzureAIVision/Services/IAzureVisionService.cs
@@ -0,0 +1,37 @@
+﻿using Azure;
+using Azure.AI.Vision.ImageAnalysis;
+using AzureAIVision.Options;
+using Microsoft.Extensions.Options;
+
+namespace AzureAIVision.Services
+{
+    public interface IAzureVisionService
+    {
+        ImageAnalysisResult AnalizeImage(Stream stream);
+    }
+
+    public class AzureVisionService : IAzureVisionService
+    {
+        private readonly ImageAnalysisClient imageAnalysisClient;
+
+        public AzureVisionService(IOptions<AzureVisionSettings> options)
+        {
+            var settings = options.Value;
+
+            this.imageAnalysisClient = new ImageAnalysisClient(
+                new Uri(settings.Endpoint),
+                new AzureKeyCredential(settings.ApiKey));
+        }
+
+        public ImageAnalysisResult AnalizeImage(Stream stream)
+        {
+            return imageAnalysisClient.Analyze(
+                BinaryData.FromStream(stream),
+                VisualFeatures.Caption |
+                VisualFeatures.DenseCaptions |
+                VisualFeatures.Tags |
+                VisualFeatures.Objects
+                );
+        }
+    }
+}
